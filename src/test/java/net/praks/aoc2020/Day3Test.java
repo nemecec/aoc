4679f33d13305c6d2d2e8cac8b335e06a9aed73d
Neeme Praks <neeme.praks@eesti.ee>
2020-12-03 10:57:40 +0200

    Day 3, part 2

--- src/test/java/net/praks/aoc2020/Day3Test.java
+++ src/test/java/net/praks/aoc2020/Day3Test.java
@@ -14,17 +14,22 @@ class Day3Test {
 
   @ParameterizedTest
   @CsvSource({
-      "part1-small, 3/1, 7",
-      "part1, 3/1, 272"
+      "part1-small, 1/1;3/1;5/1;7/1;1/2, 336",
+      "part1, 1/1;3/1;5/1;7/1;1/2, 3898725600"
   })
-  void test(String caseName, String slopeStr, int expectedTreeCount) throws IOException {
+  void test(String caseName, String slopeStr, long expectedTotalProduct) throws IOException {
     try (Reader reader = new InputStreamReader(ResourceUtil.getResource(getClass(),caseName))) {
       Day3.Map map = Day3.Map.parse(CharStreams.readLines(reader));
-      String[] slopeStrings = slopeStr.split("/");
-      int columnInc = Integer.parseInt(slopeStrings[0]);
-      int rowInc = Integer.parseInt(slopeStrings[1]);
-      int actualTreeCount = new Day3().countTreesForSlope(columnInc, rowInc, map);
-      assertThat(actualTreeCount).isEqualTo(expectedTreeCount);
+      long actualTotalProduct = 1;
+      String[] slopeStringPairs = slopeStr.split(";");
+      for (String slopeStringPair : slopeStringPairs) {
+        String[] slopeStrings = slopeStringPair.split("/");
+        int columnInc = Integer.parseInt(slopeStrings[0]);
+        int rowInc = Integer.parseInt(slopeStrings[1]);
+        int actualTreeCount = new Day3().countTreesForSlope(columnInc, rowInc, map);
+        actualTotalProduct *= actualTreeCount;
+      }
+      assertThat(actualTotalProduct).isEqualTo(expectedTotalProduct);
     }
   }
 
